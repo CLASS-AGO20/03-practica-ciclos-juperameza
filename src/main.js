9019ac4d4a589edc4ec5495b18ab98a7ec91ab5b
juperameza <juperameza@hotmail.com>
2020-12-10 16:06:05 -0600

    Added function factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-export default class App {}
+export default class App {
+    factorial(numero){
+        let resultado=1;
+        for(let i=1;i<=numero;i++){
+            resultado*=i;
+        }
+        return resultado;
+    }
+  
+
+
+
+
+
+}
 
 let app = new App();
+console.log(app.factorial(5))
+console.log(app.convertirAString(5));
